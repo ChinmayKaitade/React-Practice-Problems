f534f37ff6539506f3c396b89fca07a243f25bd5
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-16 07:22:21 +0530

    01-hello world basic project added

---
+++ 01_hello_world/src/App.jsx
@@ -0,0 +1,11 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="bg-[#313131] text-white h-screen">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_hello_world/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-center">Hello World from ReactJS</h1>
+    </div>
+  );
+};
+
+export default Hello;
